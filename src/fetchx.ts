400a124dd60b1b2b7a5de44c1d91d6c811c34a54
Kazte <39470734+Kazte@users.noreply.github.com.>
2024-08-03 15:44:53 -0300

    ✨ - add fetchx function

---
+++ src/fetchx.ts
@@ -0,0 +1,29 @@
+import { TypedRequestInit } from './types';
+
+/**
+ * A typesafe fetch function.
+ *
+ * @param input - The input URL or RequestInfo.
+ * @param init - The initialization options for the request.
+ * @returns A Promise that resolves to the typed response.
+ */
+export async function fetchx<ResponseType = any>(
+  input: RequestInfo | URL | string,
+  init?: TypedRequestInit,
+): Promise<TypedResponse<ResponseType>> {
+  if (
+    init?.method &&
+    !['POST', 'PUT', 'DELETE', 'UPDATE'].includes(init.method)
+  ) {
+    if (init.body) {
+      throw new Error(`HTTP method ${init.method} does not allow a body`);
+    }
+  }
+  const response = await fetch(input, init);
+
+  return response;
+}
+
+interface TypedResponse<T> extends Response {
+  json(): Promise<T>;
+}
